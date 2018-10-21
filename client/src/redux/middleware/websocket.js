49b8c26f5faa5a9c2a965dec81d914c937aa104d
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2018-10-21 20:15:12 +0200

    [INIT] - Project initialization

---
+++ client/src/redux/middleware/websocket.js
@@ -0,0 +1,19 @@
+import io from "socket.io-client";
+
+
+export const socket = io.connect('localhost:3000');
+
+export const socketMiddleware = function (socket) {
+    return function () {
+        return function (next) {
+            return function (action) {
+                if (action.meta && action.meta.socket && action.meta.socket.channel) {
+                    let meta = JSON.parse(JSON.stringify(action.meta));
+                    delete action.meta.socket
+                    socket.emit(meta.socket.channel, action);
+                }
+                return next(action);
+            };
+        };
+    };
+};
---
+++ client/src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import { socket, socketMiddleware } from './middleware/websocket';
+
+import rootReducer from "./reducers/index";
+
+export const store = createStore(rootReducer, applyMiddleware(socketMiddleware(socket)));
+
+socket.on('data:store', function(action) {
+   store.dispatch(action)
+});
+
