39da957103c7eeaefe32fefbf62aec87b45b7042
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2018-11-08 18:57:36 +0100

    [BACK] - Checklists features in progress

--- server/models/Checklist.js
+++ server/models/Checklist.js
@@ -1,21 +1,32 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-let checklist = new Schema({
-    name: String,
-    idBoard: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Board'
+let checklistSchema = new Schema({
+        name: String,
+        idBoard: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Board'
+        },
+        idCard: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Card'
+        },
+        pos: {
+            type: Number,
+            required: true
+        }
     },
-    idCard: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Card'
-    },
-    pos: {
-        type: Number,
-        required: true
-    }
-},
     {
+        toJSON: { virtuals: true },
         versionKey: false
     });
+
+checklistSchema.virtual('checkItems', {
+    ref: 'CheckItem', // The model to use
+    localField: '_id', // Find people where `localField`
+    foreignField: 'idChecklist' // is equal to `foreignField`
+});
+
+let Checklist = mongoose.model('Checklist', checklistSchema);
+
+module.exports = Checklist;
---
+++ server/routes/swagger/ChecklistDefinition.js
@@ -0,0 +1,7 @@
+/**
+ * @typedef Checklist
+ * @property {string} name.required - the checklist's name
+ * @property {number} pos - the checklist's pos
+ * @property {string} idCard - the checklist's card attached
+ * @property {string} idBoard - the checklist's board attached
+ */
