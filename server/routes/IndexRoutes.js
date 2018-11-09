8064ab093f2ea632d50a0047ea0d5da48d4ce501
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2018-11-09 15:31:51 +0100

    [DEPLOY] - Fix coma in the Index routes

--- server/routes/IndexRoutes.js
+++ server/routes/IndexRoutes.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', function(req, res,) {
+router.get('/', function(req, res) {
     res.send('<h1>Hello world</h1>');
 });
 
