acf2b759b944e17afb3489ef1efd8a555418c780
JohnSnoow <jean-bezet@orange.fr>
2018-11-13 13:20:37 +0100

    Request to db on show, not on mount, unsubscribe sockets on unmount

--- client/src/components/Board/List/Card/Card.jsx
+++ client/src/components/Board/List/Card/Card.jsx
@@ -60,7 +60,11 @@ export class CardComponent extends React.Component {
               </ContainerCard>
             }
           </Draggable>
-          <CardModal listId={listId} card={card} open={open} closeModal={() => this.closeModal()} />
+          {(open) ?
+            <CardModal listId={listId} card={card} open={open} closeModal={() => this.closeModal()} />
+            :
+            null
+          }
       </div>
     )
   }
